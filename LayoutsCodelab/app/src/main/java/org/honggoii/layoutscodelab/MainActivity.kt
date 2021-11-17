b672c424ba349574c3eea643caab9a97ebd7cf94
honggoii <honggoii@naver.com>
2021-11-17 22:57:58 +0900

    [Layouts in Jetpack Compose] 5. Material Components

--- LayoutsCodelab/app/src/main/java/org/honggoii/layoutscodelab/MainActivity.kt
+++ LayoutsCodelab/app/src/main/java/org/honggoii/layoutscodelab/MainActivity.kt
@@ -5,34 +5,164 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.MusicVideo
+import androidx.compose.material.icons.filled.People
+import androidx.compose.material.icons.filled.PlaylistPlay
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import org.honggoii.layoutscodelab.ui.theme.LayoutsCodelabTheme
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LayoutsCodelabTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    LayoutsCodelab()
                 }
             }
         }
+    }
+}
+
+@ExperimentalMaterialApi
+@Composable
+fun LayoutsCodelab() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "LayoutsCodelab")
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Favorite, contentDescription = null)
+                    }
+                }
+            )
+        },
+        bottomBar = {BottomNavigationBar()},
+        drawerElevation = 5.dp,
+        drawerContent = { BottomDrawer()},
+    ) {innerPadding ->
+        BodyContent(
+            Modifier
+                .padding(innerPadding)
+                .padding(8.dp))
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(text = "Hi there!")
+        Text(text = "Thanks for going through the Layouts codelab")
+
+    }
+}
+
+@Composable
+fun BottomNavigationBar() {
+    var selectedItem by remember { mutableStateOf(0) }
+    val items = listOf("Songs", "Artists", "Playlists")
+    val icons = listOf(Icons.Filled.MusicVideo, Icons.Filled.People, Icons.Filled.PlaylistPlay)
+
+    BottomNavigation {
+        items.forEachIndexed { index, item ->
+            BottomNavigationItem(
+                icon = { Icon(icons[index], contentDescription = null) },
+                label = { Text(item) },
+                selected = selectedItem == index,
+                onClick = { selectedItem = index }
+            )
+        }
+    }
+}
+
+@ExperimentalMaterialApi
+@Composable
+fun BottomDrawer() {
+    val (gesturesEnabled, toggleGesturesEnabled) = remember { mutableStateOf(true) }
+    val scope = rememberCoroutineScope()
+    Column {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .toggleable(
+                    value = gesturesEnabled,
+                    onValueChange = toggleGesturesEnabled
+                )
+        ) {
+            Checkbox(gesturesEnabled, null)
+            Text(text = if (gesturesEnabled) "Gestures Enabled" else "Gestures Disabled")
+        }
+        val drawerState = rememberBottomDrawerState(BottomDrawerValue.Closed)
+        BottomDrawer(
+            gesturesEnabled = gesturesEnabled,
+            drawerState = drawerState,
+            drawerContent = {
+                Button(
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(top = 16.dp),
+                    onClick = { scope.launch { drawerState.close() } },
+                    content = { Text("Close Drawer") }
+                )
+                LazyColumn {
+                    items(25) {
+                        ListItem(
+                            text = { Text("Item $it") },
+                            icon = {
+                                Icon(
+                                    Icons.Default.Favorite,
+                                    contentDescription = "Localized description"
+                                )
+                            }
+                        )
+                    }
+                }
+            },
+            content = {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    val openText = if (gesturesEnabled) "▲▲▲ Pull up ▲▲▲" else "Click the button!"
+                    Text(text = if (drawerState.isClosed) openText else "▼▼▼ Drag down ▼▼▼")
+                    Spacer(Modifier.height(20.dp))
+                    Button(onClick = { scope.launch { drawerState.open() } }) {
+                        Text("Click to open")
+                    }
+                }
+            }
+        )
+    }
+}
+
+@ExperimentalMaterialApi
+@Preview
+@Composable
+fun LayoutsCodelabPreview() {
+    LayoutsCodelabTheme {
+        LayoutsCodelab()
     }
 }
 
@@ -74,15 +204,15 @@ fun PhotographerCardPreview() {
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+//@Composable
+//fun Greeting(name: String) {
+//    Text(text = "Hello $name!")
+//}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LayoutsCodelabTheme {
-        Greeting("Android")
-    }
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    LayoutsCodelabTheme {
+//        Greeting("Android")
+//    }
+//}
