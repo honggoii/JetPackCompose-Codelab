1b52fd68f34d182c0015b8d36a49bde0933dba1c
honggoii <honggoii@naver.com>
2021-11-20 18:16:01 +0900

    [Layouts in Jetpack Compose] 10. Constraint Layout

--- LayoutsCodelab/app/src/main/java/org/honggoii/layoutscodelab/MainActivity.kt
+++ LayoutsCodelab/app/src/main/java/org/honggoii/layoutscodelab/MainActivity.kt
@@ -21,14 +21,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.AlignmentLine
-import androidx.compose.ui.layout.FirstBaseline
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.layout.layout
+import androidx.compose.ui.layout.*
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
+import androidx.constraintlayout.compose.atLeast
 import coil.compose.rememberImagePainter
 import kotlinx.coroutines.launch
 import org.honggoii.layoutscodelab.ui.theme.LayoutsCodelabTheme
@@ -44,6 +45,102 @@ class MainActivity : ComponentActivity() {
                     LayoutsCodelab()
                 }
             }
+        }
+    }
+}
+
+@ExperimentalMaterialApi
+@Preview
+@Composable
+fun LayoutsCodelabPreview() {
+    LayoutsCodelabTheme {
+        LargeConstraintLayout()
+        //ConstraintLayoutContent()
+        //BodyContent()
+        //LayoutsCodelab()
+    }
+}
+
+@Composable
+fun DecoupledConstraintLayout() {
+    BoxWithConstraints {
+        val constraints = if (maxWidth < maxHeight) {
+            decoupledConstraints(margin = 16.dp)
+        } else {
+            decoupledConstraints(margin = 32.dp)
+        }
+
+        ConstraintLayout(constraints) {
+            Button(
+                onClick = { /* Do something */ },
+                modifier = Modifier.layoutId("button")
+            ) {
+                Text("Button")
+            }
+
+            Text("Text", Modifier.layoutId("text"))
+        }
+    }
+}
+
+private fun decoupledConstraints(margin: Dp) : ConstraintSet {
+    return ConstraintSet{
+        val button = createRefFor("button")
+        val text = createRefFor("text")
+
+        constrain(button) {
+            top.linkTo(parent.top, margin = margin)
+        }
+        constrain(text) {
+            top.linkTo(button.bottom, margin)
+        }
+    }
+}
+
+@Composable
+fun LargeConstraintLayout() {
+    ConstraintLayout {
+        val text = createRef()
+
+        val guideline = createGuidelineFromStart(fraction = 0.5f)
+        Text(
+            "This is a very very very very very very very very very very very very  long text",
+            Modifier.constrainAs(text) {
+                linkTo(start = guideline, end = parent.end)
+                width = Dimension.preferredWrapContent.atLeast(100.dp)
+            }
+        )
+    }
+}
+
+@Composable
+fun ConstraintLayoutContent(){
+    ConstraintLayout{
+        val (button1, button2, text) = createRefs() // Create references
+
+        Button(
+            onClick = { /* Do something */},
+            modifier = Modifier.constrainAs(button1) {
+                top.linkTo(parent.top, margin = 16.dp)
+            }
+        ) {
+            Text("Button1")
+        }
+
+        Text("Text", Modifier.constrainAs(text) {
+            top.linkTo(button1.bottom, margin = 16.dp)
+            centerAround(button1.end)
+        })
+
+        val barrier = createEndBarrier(button1, text)
+        Button(
+            onClick = { /* Do something */},
+            modifier = Modifier.constrainAs(button2) {
+                top.linkTo(parent.top, margin = 16.dp)
+                start.linkTo(barrier)
+            }
+        ) {
+            Text("Button2")
         }
     }
 }
@@ -393,16 +490,6 @@ fun BottomDrawer() {
                 }
             }
         )
-    }
-}
-
-@ExperimentalMaterialApi
-@Preview
-@Composable
-fun LayoutsCodelabPreview() {
-    LayoutsCodelabTheme {
-        BodyContent()
-        //LayoutsCodelab()
     }
 }
 
